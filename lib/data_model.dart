de5b85398c741eeae7a94e0c8fd7834e60bab579
Oussama Maatallah <oussamamaatallah01@gmail.com>
2021-02-21 18:04:21 +0100

    added extension methods and some documentation.

--- lib/data_model.dart
+++ lib/data_model.dart
@@ -8,44 +8,61 @@ import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
 
 abstract class DataModel {
+  /// This method takes a Map<String,dynamic> as an argument and applies
+  /// it's data to the DataModel.
   void fromMap(Map<String, dynamic> map);
+
+  /// This method returns DataModel's data as a Map of <String,dynamic>.
   Map<String, dynamic> toMap();
-  String toPath();
-  DateTime creationDate;
+
+  /// This field is the id of the DataModel.
   String id;
-  Future save();
-  Future load();
+
+  /// This field gives you the creationDate of the current DataModel.
+  /// (This field is used to update your DataModel safely, it compares the
+  /// creationDate of the saved DataModel and the creationDate of the
+  /// current DataModel)
+  DateTime creationDate;
 }
 
-Future<void> saveModel(DataModel model) async {
-  String path = (await getApplicationDocumentsDirectory()).path;
-  var file = File(join(path, model.toPath() + '.json'));
-  model.creationDate = model.creationDate ?? DateTime.now();
-  file.createSync(recursive: true);
-  file.writeAsStringSync(jsonEncode(model.toMap()));
-  var db = await loadDB();
-  if (db.containsKey(model.toString())) {
-    db[model.toString()].add(model.id);
-  } else {
-    db[model.toString()] = [];
-    db[model.toString()].add(model.id);
+extension CRUD on DataModel {
+  /// This method let's you save your model locally.
+  Future<void> save() async {
+    String path = (await getApplicationDocumentsDirectory()).path;
+    var file = File(join(path, 'db', toPath() + '.json'));
+    creationDate = creationDate ?? DateTime.now();
+    file.createSync(recursive: true);
+    file.writeAsStringSync(jsonEncode(toMap()));
+    var db = await loadDB();
+    if (db.containsKey(toString())) {
+      db[toString()].add(id);
+    } else {
+      db[toString()] = [];
+      db[toString()].add(id);
+    }
+    var fileDB = File(join(path, 'db', 'db.json'));
+    fileDB.writeAsStringSync(jsonEncode(db));
   }
-  var fileDB = File(join(path, 'db.json'));
-  fileDB.writeAsStringSync(jsonEncode(db));
+
+  /// This method lets you load your model from local storage.
+  Future<void> load() async {
+    String path = (await getApplicationDocumentsDirectory()).path;
+    var file = File(join(path, 'db', toPath() + '.json'));
+    var dataString = file.readAsStringSync();
+    fromMap(jsonDecode(dataString));
+  }
+
+  /// This method gives you the path of your DataModel
+  String toPath() => '${toString()}/$id';
 }
 
-Future<DataModel> loadModel(DataModel model) async {
-  String path = (await getApplicationDocumentsDirectory()).path;
-  var file = File(join(path, model.toPath() + '.json'));
-  var dataString = file.readAsStringSync();
-  model.fromMap(jsonDecode(dataString));
-  return model;
-}
-
+/// This method let's you load your DataModels local Database
 Future<Map<String, dynamic>> loadDB() async {
   String path = (await getApplicationDocumentsDirectory()).path;
   try {
-    var file = File(join(path, 'db.json'));
+    var file = File(join(path, 'db', 'db.json'));
+    file.createSync(recursive: true);
+    if (file.readAsStringSync() == '') return {};
     var data = jsonDecode(file.readAsStringSync());
     return data;
   } catch (e) {
